195d1a186de7a29ab3c0b05b882ed873e929398a
Gaurav Joshi <79218309+gauravmbjoshi@users.noreply.github.com>
2025-07-15 09:16:29 +0530

    register an "update event" route and handler

--- routes/events.go
+++ routes/events.go
@@ -47,3 +47,29 @@ func createEvent(c *gin.Context) {
 
     c.JSON(http.StatusCreated, gin.H{"message": "Event created successfully!", "event": event})
 }
+
+func updateEvent(context *gin.Context){
+	eventId,err := strconv.ParseInt(context.Param("id"),10,64)
+	if err != nil{
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+    _,err = models.GetEventById(eventId)
+    if err !=nil{
+        context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+    }
+	var updateEvent models.Event
+	err = context.ShouldBindJSON(&updateEvent);
+	if err != nil {
+        context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event format."})
+        return
+    }
+	updateEvent.ID = eventId
+	err = updateEvent.Update()
+	if err != nil {
+        context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event."})
+        return
+    }
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully!"})
+}
--- routes/routes.go
+++ routes/routes.go
@@ -7,5 +7,5 @@ func RegisterRoutes(server *gin.Engine) { // as we are pointing to the server we
 	server.GET("/events", getEvents)
     server.GET("/events/:id", getEvent) // dynamic path handler setup using /:id
     server.POST("/events", createEvent)
-
+	server.PUT("/events/:id",updateEvent)
 }
